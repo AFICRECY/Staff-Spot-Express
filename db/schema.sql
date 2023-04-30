6222813dc1235bbd4fdcb9e606d4359859f019b2
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-04-30 00:02:03 -0700

    input data for the tables

--- db/schema.sql
+++ db/schema.sql
@@ -22,11 +22,11 @@ CREATE TABLE roles (
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30)
-    title VARCHAR(30)
-    department VARCHAR(50)
-    salary VARCHAR(30)
-    manager VARCHAR(30)
+    last_name VARCHAR(30),
+    title VARCHAR(30),
+    department VARCHAR(50),
+    salary VARCHAR(30),
+    manager VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,31 @@
-INSERT INTO Departments (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+INSERT INTO Departments (dept_id, dept_name)
+VALUES (1, "Sales"),
+    (2, "Human Resources"),
+    (3, "Engineering"),
+    (4, "Finance");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+
+
+
+
+INSERT INTO  roles (role_id, title, department, salary)
+VALUES (1, "Sales Lead", "Sales", "100000"),
+    (2, "Salesperson", "Sales", "80000"),
+    (3, "HR Director", "Human Resources", "130000"),
+    (4, "HR Sepecialist", "Human Resources", "80000"),
+    (5, "Senior Software Engineer", "Engineering", "250000"),
+    (6, "Associate Software Engineer", "Engineering", "100000"),
+    (7, "Finance Director", "Finance", "140000"),
+    (8, "Accountant", "Finance", "90000");
+
+
+
+INSERT INTO  employees (employee_id, firstName, lastName, title, department, salary, manager)
+VALUES (1, "Naomi", "Johnson", "Sales Lead", "Sales", "100000", Null),
+    (2, "Marcus", "White", "Salesperson", "Sales", "80000", "Sara Grace"),
+    (3, "Aktunde", "Ahmad" "HR Director", "Human Resources", "130000", Null),
+    (4, "Jamar", "Sanders", "HR Sepecialist", "Human Resources", "80000", "Aktunde Ahmad"),
+    (5, "Tamika", "Johnson", "Senior Software Engineer", "Engineering", "250000", Null),
+    (6, "Rashon", "Davis", "Associate Software Engineer", "Engineering", "100000", "Tamika Johnson"),
+    (7, "Marques", "Houston", "Finance Director", "Finance", "140000", Null),
+    (8, "Salim" "Gupta" "Accountant", "Finance", "90000", "Marques Houston");
