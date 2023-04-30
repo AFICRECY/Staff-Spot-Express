6266a33c0809400d6aaae6173a25045d9a3d05a2
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-04-30 00:05:20 -0700

    updated the id name for each table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     employee_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     department VARCHAR(30) NOT NULL,
     salary VARCHAR(30) NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30),
-    title VARCHAR(30),
-    department VARCHAR(50),
-    salary VARCHAR(30),
+    last_name VARCHAR(30) NOT NULL,
+    title VARCHAR(30), NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    salary VARCHAR(30) NOT NULL,
     manager VARCHAR(30),
-    role_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id)
     ON DELETE SET NULL
 );
