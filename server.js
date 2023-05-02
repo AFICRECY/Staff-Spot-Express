68e52c492427544289aa68c067d537ce2d590526
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-01 19:02:27 -0700

    working on the add employee function

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 // const { throwError } = require('rxjs');
 
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // MySQL username,
-    user: 'root',
-    password: 'password',
-    database: 'employee_db'
+    user: "root",
+    password: "password",
+    database: "employee_db",
   },
   console.log(`Connected to the employee_db database.`)
 );
-db.connect((error) => {
-  if (error) throw error
 
-  promptStart()
-}) 
+db.connect((error) => {
+  if (error) throw error;
+
+  promptStart();
+});
 
 var figlet = require("figlet");
 
@@ -30,199 +31,240 @@ figlet("Staff Spot Express", function (err, data) {
   console.log(data);
 });
 
-
-
-
-
-
 const homeQuestions = [
   {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'homeOptions',
-      choices: ['View All Employees', 'Add Employee','Update Employee Role','View All Roles', 'Add Role','View All Departments', 'Add Department'],
-  }
-  ]
+    type: "list",
+    message: "What would you like to do?",
+    name: "homeOptions",
+    choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+    ],
+  },
+];
 
-  const promptStart = function () {
-    inquirer.prompt(homeQuestions).then((response) => {
-      if(response.homeOptions === "View All Employees"){
-        allEmployees()
-      } else if (response.homeOptions === "Add Employee"){
-        addEmployee()
-      }else if (response.homeOptions === "UpdateEmployee Role"){
-        updateEmployees()
-      }else if (response.homeOptions === "View All Roles"){
-        allRoles()
-      }else if (response.homeOptions === "Add Role"){
-        addRole()
-      }else if (response.homeOptions === "View All Departments"){
-        allDepartments()
-      }else if (response.homeOptions === "Add Department"){
-        addDepartments()
+const promptStart = function () {
+  inquirer.prompt(homeQuestions).then((response) => {
+    if (response.homeOptions === "View All Employees") {
+      allEmployees();
+    } else if (response.homeOptions === "Add Employee") {
+      addEmployee();
+    } else if (response.homeOptions === "UpdateEmployee Role") {
+      updateEmployees();
+    } else if (response.homeOptions === "View All Roles") {
+      allRoles();
+    } else if (response.homeOptions === "Add Role") {
+      addRole();
+    } else if (response.homeOptions === "View All Departments") {
+      allDepartments();
+    } else if (response.homeOptions === "Add Department") {
+      addDepartments();
     }
-  })
-  }
-
+  });
+};
 
 //   WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 function allDepartments() {
-  db.query("SELECT * FROM department", (error, response)=> {
-    if (error) throw error
-    console.table(response)
-    promptStart()
-  })
+  db.query("SELECT * FROM department", (error, response) => {
+    if (error) throw error;
+    console.table(response);
+    promptStart();
+  });
 }
 
 function allEmployees() {
-  db.query("SELECT * FROM employee", (error, response)=> {
-    if (error) throw error
-    console.table(response)
-    promptStart()
-})
+  db.query("SELECT * FROM employee", (error, response) => {
+    if (error) throw error;
+    console.table(response);
+    promptStart();
+  });
 }
 
 function allRoles() {
-  db.query("SELECT * FROM role", (error, response)=> {
-    if (error) throw error
-    console.table(response)
-    promptStart()
-})
+  db.query("SELECT * FROM role", (error, response) => {
+    if (error) throw error;
+    console.table(response);
+    promptStart();
+  });
 }
 
-
 function addRole() {
-    inquirer.prompt([
+  inquirer
+    .prompt([
       {
-      type: 'input',
-      message: 'What is the name of the role you want to add?',
-      name: 'roleTitle',
+        type: "input",
+        message: "What is the name of the role you want to add?",
+        name: "roleTitle",
       },
       {
-      type: 'input',
-      message: 'What is the salary for that role?',
-      name: 'roleSalary',
+        type: "input",
+        message: "What is the salary for that role?",
+        name: "roleSalary",
       },
       {
-        type: 'list',
-        message: 'Which department does this role belong to?',
-        name: 'roleDept',
-        choices: ['Sales', 'Human Resources','Engineering','Finance'],
-      }
+        type: "list",
+        message: "Which department does this role belong to?",
+        name: "roleDept",
+        choices: ["Sales", "Human Resources", "Engineering", "Finance"],
+      },
     ])
-    .then((response,choices) => {
-      console.log(response)
-      const id_query=`SELECT id FROM role ORDER BY id DESC LIMIT 1`;
+    .then((response, choices) => {
+      console.log(response);
+      const id_query = `SELECT id FROM role ORDER BY id DESC LIMIT 1`;
       var newId;
       db.query(id_query, (err, res) => {
         if (err) {
           console.error(err);
           return;
         }
-      
-        const newId = res[0].id + 1;      
-        response.id = newId.toString();      
+
+        const newId = res[0].id + 1;
+        response.id = newId.toString();
         // Here you can put the code that inserts the new record with the new ID.
-        var choices = ['Sales', 'Human Resources','Engineering','Finance']
-        const insertRole = `INSERT INTO role (id, title, salary, department_id) VALUES (${response.id}, "${response.roleTitle}", ${response.roleSalary}, ${choices.indexOf(response.roleDept)})`;
-        db.query(insertRole,(err,res)=>{
-          if(err){
-            console.log(err)
+        var choices = ["Sales", "Human Resources", "Engineering", "Finance"];
+        const insertRole = `INSERT INTO role (id, title, salary, department_id) VALUES (${
+          response.id
+        }, "${response.roleTitle}", ${response.roleSalary}, ${choices.indexOf(
+          response.roleDept
+        )})`;
+        db.query(insertRole, (err, res) => {
+          if (err) {
+            console.log(err);
           }
-        })
-        db.query("SELECT * FROM role", (error, response)=> {
-          if (error) throw error
-          console.table(response)
-          promptStart()
-        })
+        });
+        db.query("SELECT * FROM role", (error, response) => {
+          if (error) throw error;
+          console.table(response);
+          promptStart();
+        });
       });
-      
-    })
+    });
 }
 
-function addDepartments(){
-  inquirer.prompt([
+function addDepartments() {
+  inquirer
+    .prompt([
       {
-      type: 'input',
-      message: 'What is the name of the Department you want to add?',
-      name: 'depTitle',
-      }
+        type: "input",
+        message: "What is the name of the Department you want to add?",
+        name: "depTitle",
+      },
     ])
     .then((response) => {
-      console.log(response)
-        const id_query=`SELECT id FROM department ORDER BY id DESC LIMIT 1`;
-        var newId;
-        db.query(id_query, (err, res) => {
-          if (err) {
-            console.error(err);
-            return;
+      console.log(response);
+      const id_query = `SELECT id FROM department ORDER BY id DESC LIMIT 1`;
+      var newId;
+      db.query(id_query, (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-        const newId = res[0].id + 1;  
+        const newId = res[0].id + 1;
         response.id = newId.toString();
         const insertDept = `INSERT INTO department (id, department_name) VALUES (${response.id}, "${response.depTitle}")`;
-        db.query(insertDept,(err,res)=>{
-          if(err){
+        db.query(insertDept, (err, res) => {
+          if (err) {
             console.log(err);
             return;
           }
-        })
-        db.query("SELECT * FROM department", (error, response)=> {
-          if (error) throw error
-          console.table(response)
-          promptStart()
-        })
-    
-      })
-    })
-  }
-  
-  
-  function addEmployee(){
-    inquirer.prompt([
-        {
-          type: 'input',
-          message: 'What is the first name of the employee you want to add?',
-          name: 'firstName',
-          },
-          {
-          type: 'input',
-          message: 'What is the last name of the employee you want to add?',
-          name: 'lastName',
-          },
-          {
-          type: 'list',
-          message: 'Who is this employees manager?',
-          name: 'staffManager',
-          choices: ['Naomi Johnson', 'Aktunde Ahmad','Tamika Johnson','Rashon Davis'],
-          }
-    ]) 
-    .then((response,choices)=>{
-      console.log(response)
-      const id_query=`SELECT id FROM employee ORDER BY id DESC LIMIT 1`;
-        var newId;
-        db.query(id_query,(err, res)=>{
-          if (err) {
-            console.error(err);
-            return;
-          }
-          const newId = res[0].id + 1;  
-          response.id = newId.toString();
-          var choices = ['Naomi Johnson', 'Aktunde Ahmad','Tamika Johnson','Rashon Davis'],
-          // const insertDept = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (${response.id}, "${response.firstName}", "${response.lastName}", "${response.staffManager}"," ", "${choices.indexOf(response.staffManager)}"`;})
-        })
-  })
-  
+        });
+        db.query("SELECT * FROM department", (error, response) => {
+          if (error) throw error;
+          console.table(response);
+          promptStart();
+        });
+      });
+    });
 }
-  // addDepartments()
-  // addEmployee()
-  // updateEmployee()
+
+function returnArrayOfAllRoles() {
+  db.query("SELECT title FROM role", (error, response) => {
+    if (error) throw error;
+    //console.log('response is', response);
+    // map over the response array that we get back from querying
+    // and return the value of the title property
+    const arrayOfRoleChoices = response.map((role) => {
+      return role.title;
+    });
+    // return your array of roles
+    console.log("role array is", arrayOfRoleChoices);
+    return arrayOfRoleChoices;
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the first name of the employee you want to add?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the last name of the employee you want to add?",
+        name: "lastName",
+      },
+      {
+        type: "list",
+        message: "What is this employee's role?",
+        name: "staffRole",
+        choices: ['Manager', 'Sales Representative', 'Marketing Manager', 'Marketing Specialist', 'Software Engineer', 'Associate Engineer', 'Associate Engineer', 'Product Designer']
+        // choices: async () => {
+        //   // the purpose of this function is to dynamically retrieve all of the existing roles
+        //   // in the role datatable by executing the sql query below. Then you map over the response
+        //   // that you get to simply generate an array of string values (where values are the roles)
+        //     db.query("SELECT title FROM role", (error, response) => {
+        //     if (error) throw error;
+        //     const arrayOfRoleChoices = response.map((role) => {
+        //       return role.title;
+        //     });
+        //     return arrayOfRoleChoices;
+        //   });
+        // }
+      },
+      {
+        type: "list",
+        message: "Who is this employee's manager?",
+        name: "staffManager",
+        choices: [
+          "Naomi Johnson",
+          "Aktunde Ahmad",
+          "Tamika Johnson",
+          "Rashon Davis",
+        ],
+      },
+    ])
+    .then((response, choices) => {
+      console.log(response);
+      const id_query = `SELECT id FROM employee ORDER BY id DESC LIMIT 1`;
+      var newId;
+      db.query(id_query, (err, res) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const newId = res[0].id + 1;
+        response.id = newId.toString();
+        var choices = [
+          "Naomi Johnson",
+          "Aktunde Ahmad",
+          "Tamika Johnson",
+          "Rashon Davis",
+        ];
+        const insertDept = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (${response.id}, "${response.firstName}", "${response.lastName}", "${response.staffManager}", "${choices.indexOf(response.staffRole)}", "${choices.indexOf(response.staffManager)}"`;})
+      });
+    };
   
-  
-
-
-
-
+// addDepartments()
+// addEmployee()
+// updateEmployee()
 
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
@@ -240,19 +282,12 @@ function addDepartments(){
 // // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 // addEmployee()
 // // WHEN I choose to update an employee role
-// // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+// // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 // updateEmployee()
-
-
-
-
-
-
 
 // SELECT *
 // FROM roles,
 // INNER JOIN title ON department.roles = title.id;
-
 
 // SELECT
 //     firstName AS first_name, lastName.last_name, title.last_name AS last_name, title.Position, department.Position AS Position, department.department, salary.department AS department, salary.salary, salary.manager AS manager
@@ -260,7 +295,7 @@ function addDepartments(){
 // JOIN allRoles ON allDepartments. = allRoles.id;
 
 // Update review name
-// PUT 
+// PUT
 // http://localhost:3001/api/review/1
 // {
 // "movie": "Lion King",
@@ -273,7 +308,6 @@ function addDepartments(){
 
 //   db.query(sql, params, (err, result) => {
 //     if (err) {
-
 
 //       res.status(400).json({ error: err.message });
 //     } else if (!result.affectedRows) {
@@ -289,4 +323,3 @@ function addDepartments(){
 //     }
 //   });
 // });
-
